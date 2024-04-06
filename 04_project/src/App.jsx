574e16cee980d51abf1c2bffa7ade771c60119f8
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-04-06 17:49:56 +0530

    updte

--- 04_project/src/App.jsx
+++ 04_project/src/App.jsx
@@ -7,11 +7,17 @@ import Card from "./components/Card";
 function App() {
   const [count, setCount] = useState(0);
 
+  const obj = {
+    username : "Rahul",
+    roll : "21",
+  }
+
   return (
     <>
       <h1 className="bg-green-400 p-4 rounded-xl mb-5">TailwindCSS</h1>
-     <Card/>
-     <Card/>
+     <Card username="Emma" btnText="Click mee"/>
+     <Card username="Georgia" btnText="Explore mee"/>
+     <Card username="Georgia"/>
     </>
   );
 }
