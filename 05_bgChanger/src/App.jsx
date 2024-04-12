239f2a68f01d7db94b9cc498f3533552eb9e3c8c
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-04-12 15:47:55 +0530

    Update App.jsx

--- 05_bgChanger/src/App.jsx
+++ 05_bgChanger/src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+<<<<<<< HEAD
+  const [color, setColor] = useState("black");
+
+  function changeColor(color) {
+    setColor(color);
+  }
+
+
+  return (
+    <div className="w-full h-screen"
+      style={{ backgroundColor : color }}>Home
+      <br />
+      <button>Red</button>
+      <button>Green</button>
+      <button>Blue</button>
+      <button>White</button>
+      <button>Gulambi</button>
+    </div>
+=======
   const [count, setCount] = useState(0)
   const [color, setColor] = useState("white");
 
@@ -11,6 +28,7 @@ function App() {
     <>
      <div className="bg-${color} h-screen w-screen text-black flex justify-center items-center">Hello</div>
     </>
+>>>>>>> f61c8e43c63088e2ed9e953f568fba34e30682b1
   )
 }
 
