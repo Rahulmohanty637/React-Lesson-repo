16678e048e1a42956a66d02f77b6e7ada04fb551
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-04-16 12:16:42 +0530

    Update App.jsx

--- 05_password_Generator/src/App.jsx
+++ 05_password_Generator/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -9,11 +9,27 @@ function App() {
   const [charalw, setCharalw] = useState(false);
   const [password, setPassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numalw) str+= "0123456789";
+    if(charalw) str+= "!@#$%^&*_+=[]{}~`";
+    for (let index = 0; index < array.length; index++) {
+      
+      
+    }
+  }, [
+    length,
+    numalw,
+    charalw,
+    setPassword,
+  ]);
+
   return (
     <>
       <h1>Password Generator</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
