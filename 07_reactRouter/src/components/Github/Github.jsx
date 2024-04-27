7ee8e071bf8ed267ef7cd08cc2b12207b949c9d1
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-04-27 14:23:41 +0530

    update

--- 07_reactRouter/src/components/Github/Github.jsx
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router'
 
 const Github = () => {
 
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch('https://api.github.com/users/Rahulmohanty637')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      setData(data);
-    })
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://api.github.com/users/Rahulmohanty637')
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //     setData(data);
+  //   })
+  // }, [])
+
+  const data =  useLoaderData(githubInfoLoader);
+
+
   return (
     <div className='text-center'>Github followers : {data.followers}
     <img src={data.avatar_url} alt='profile picture'></img>
@@ -20,3 +25,8 @@ const Github = () => {
 }
 
 export default Github
+
+export const githubInfoLoader = async() => {
+  const response = await fetch("https://api.github.com/users/Rahulmohanty637");
+  return response.json();
+}
