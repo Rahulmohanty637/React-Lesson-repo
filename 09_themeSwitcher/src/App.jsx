b232a5e2ab17e8502964216b6a68d15cbd0ea0da
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2024-06-12 19:22:03 +0530

    updated

--- 09_themeSwitcher/src/App.jsx
+++ 09_themeSwitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./Context/Theme";
+import ThemeBtn from "./Components/ThemeButton";
+import Card from "./Components/Card";
 
 function App() {
   const [themMode, setThemeMode] = useState("light");
@@ -15,16 +17,23 @@ function App() {
 
   // actual change in theme
   useEffect(() => {
-    const html = document.querySelector('html').classList.remove("light", "dark");
+    const html = document
+      .querySelector("html")
+      .classList.remove("light", "dark");
     html.classList.add("dark");
-  },[themMode]);
+  }, [themMode]);
+  
   return (
     <ThemeProvider value={{ themMode, darkThemeColor, lightThemeColor }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-          <div className="w-full max-w-sm mx-auto"></div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
